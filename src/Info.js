0bdd38faca31e42b35097b7c822a328364ceec16
chueunjeong <miho7724@gmail.com>
2020-09-19 19:54:29 +0900

    [shin]update

--- src/Info.js
+++ src/Info.js
@@ -1,17 +1,98 @@
 import React from 'react';
-import {Container, Content, Text, Button} from 'native-base';
+import {Container, Content, Text, TextInput} from 'native-base';
 import Footer from './Footer';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+  ImageBackground,
+  ScrollView,
+} from 'react-native';
 
 export default function Course({navigation}) {
   return (
     <Container>
       <Content>
-        <Button onPress={() => navigation.navigate('Prepare')}>
-          <Text>탐험 정보 상세</Text>
-        </Button>
+        <View style={styles.header}>
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: 'bold',
+              textAlign: 'center',
+              color: '#FFFFFF',
+            }}>
+            코스 선택하기
+          </Text>
+        </View>
+        <ScrollView>
+          <TouchableOpacity onPress={() => navigation.navigate('Prepare')}>
+            <Image
+              style={styles.img}
+              source={require('./img/성판악탐방로.jpg')}></Image>
+          </TouchableOpacity>
+          <ImageBackground
+            source={require('./img/성판악탐방로.jpg')}
+            style={styles.img}>
+            <View
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text style={styles.text}>>성판악 코스 시작</Text>
+            </View>
+          </ImageBackground>
+          <TouchableHighlight>
+            <Image
+              style={styles.img}
+              source={require('./img/관음사탐방로.jpg')}
+            />
+          </TouchableHighlight>
+          <TouchableHighlight>
+            <Image
+              style={styles.img}
+              source={require('./img/어리목탐방로.jpg')}
+            />
+          </TouchableHighlight>
+        </ScrollView>
       </Content>
 
       <Footer navigation={navigation} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  img: {
+    height: 250,
+    width: 350,
+    alignItems: 'center',
+    borderRadius: 3,
+    resizeMode: 'contain',
+    marginLeft: 30,
+  },
+  header: {
+    padding: 30,
+    marginBottom: 20,
+    backgroundColor: '#87D37C',
+  },
+  text: {
+    color: '#FFFFFF',
+    textAlign: 'right',
+    textAlignVertical: 'bottom',
+    fontSize: 20,
+    fontWeight: '100',
+  },
+});
--- src/Prepare.js
+++ src/Prepare.js
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#969FAA',
     marginBottom: 20,
-    marginTop: 40,
+    marginTop: 30,
   },
   Title: {
     fontSize: 36,
@@ -71,7 +71,11 @@ const styles = StyleSheet.create({
   },
   List: {
     paddingTop: 0,
-    paddingBottom: 0,
+    paddingBottom: 10,
+    borderColor: '#181817',
+    borderTopWidth: 2,
+    borderRadius: 1,
+    borderStyle: 'dotted',
   },
   ListText: {
     fontSize: 24,
@@ -102,8 +106,11 @@ const styles = StyleSheet.create({
     elevation: 1,
   },
   borderBox: {
-    borderRadius: 5,
-    backgroundColor: '#f4d03f',
-    height: '90%',
+    borderRadius: 1,
+    borderColor: '#181817',
+    margin: 20,
+    //borderTopWidth: 1,
+    borderWidth: 2,
+    borderStyle: 'dotted',
   },
 });
