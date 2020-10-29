a52f190eb95e4149722e05cbc08b8cfdb2873235
ssc94 <scrway15@gmail.com>
2020-10-29 17:27:10 +0900

    결제팝업창 수정

--- src/popup/Point.js
+++ src/popup/Point.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import PointYes from './PointYes';
 
 
@@ -39,17 +40,18 @@ const Point = ({token,price}) => {
         }}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>선택하신 포인트 충전을 진행 하시겠습니까?</Text>
+            <Text style={styles.modalText}>선택하신 포인트 충전을</Text><Text> 진행 하시겠습니까?</Text>
             <Text>{"\n"}충전 포인트 : {token}백록</Text>
             <Text styles={[(price) && {color:'red'}]}>{"\n"}결제 금액 : {price}원</Text>
             <View style={styles.button}>
-              <TouchableHighlight style={styles.openButton} onPress={() => {
+              <TouchableHighlight style={styles.selectBtn} onPress={() => {
                 chargeToken();
                 setModalVisible(!modalVisible);
-              }} ><Text>예</Text></TouchableHighlight>
-              <TouchableHighlight style={styles.openButton} onPress={() => {
+                Alert.alert('포인트 충전이 완료되었습니다.');
+              }} ><Text style={styles.textStyle}>예</Text></TouchableHighlight>
+              <TouchableHighlight style={styles.selectBtn} onPress={() => {
                 setModalVisible(!modalVisible);
-              }} ><Text>아니오</Text></TouchableHighlight>
+              }} ><Text style={styles.textStyle}>아니오</Text></TouchableHighlight>
             </View>
           </View>
         </View>
@@ -74,7 +76,8 @@ const styles = StyleSheet.create({
     marginTop: 0,
   },
   modalView: {
-    margin: 20,
+    margin: hp('10%'),
+    marginBottom:hp('30%'), 
     backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
@@ -90,14 +93,21 @@ const styles = StyleSheet.create({
   },
   openButton: {
     backgroundColor: 'green',
-    borderRadius: 20,
+    borderRadius: 15,
     padding: 15,
     elevation: 2,
     marginTop:25,
     // borderColor:'Black'
   },
+  selectBtn : {
+    backgroundColor: 'green',
+    borderRadius: 15,
+    padding:10 ,
+    margin:hp('3%'),
+  },
   button:{
-    flex:2
+    
+    flexDirection : 'row',
   },
   textStyle: {
     color: 'white',
@@ -107,6 +117,7 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: 'center',
+    
   },
 });
 
